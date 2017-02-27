9110eff33d5658ffa3397adc9bbabb6c4f77039b
Alex Tsui <alextsui05@gmail.com>
2017-02-27 11:51:39 -0800

    Demonstrate save in rulers 7.4

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -6,11 +6,9 @@ STDERR.puts MyTable.schema.inspect
 
 # Create row
 mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "I really did!"
+mt.save!
 
-puts "Count: #{MyTable.count}"
+mt2 = MyTable.find mt["id"]
 
-top_id = mt["id"].to_i
-(1..top_id).each do |id|
-  mt_id = MyTable.find(id)
-  puts "Found title #{mt_id["title"]}."
-end
+puts "Title: #{mt2["title"]}"
